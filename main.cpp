09e3e3af3bf76d749d24022008823d7243dc3a7b
MohammadMahdiRazavi <mohammed.razavi1384@gmail.com>
2024-08-08 03:49:59 -0700

    first part of menu is done

--- main.cpp
+++ main.cpp
@@ -17,19 +17,36 @@ struct gameHistory{
     int time;
 };
 
+int count_of_game = 0;
+
 void menu(int** game, int row, int column, gameHistory leaderboard[100]){
     cout << "Hi ! Welcome to Tetris";
     sleep(1000);
     system("cls");
     while (true){
-        cout << "1 - New Game";
-        cout << "2 - Leaderboard";
-        cout << "3 - How to playing Tetris";
-        cout << "4 - Exit";
+        cout << "1 - New Game" << "\n";
+        cout << "2 - Leaderboard" << "\n";
+        cout << "3 - How to playing Tetris" << "\n";
+        cout << "4 - Exit" << "\n";
         int section;
         cin >> section;
         if(section == 1){
-
+            system("cls");
+            cout << "Welcome to new game";
+            sleep(1000);
+            system("cls");
+            cout << "Please enter your name : ";
+            cin >> leaderboard[count_of_game].name;
+            system("cls");
+            cout << "Please enter the number of rows : ";
+            cin >> row;
+            sleep(500);
+            cout << "\n" << "Please enter the number of columns : ";
+            cin >> column;
+            system("cls");
+            cout << "GAME Loading ....";
+            sleep(1000);
+            system("cls");
         }
         else if(section == 2){
 
