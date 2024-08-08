739ff189befe16590e0e9bd8d8b3be7b4c7748e8
MohammadMahdiRazavi <mohammed.razavi1384@gmail.com>
2024-08-08 03:08:40 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+#include <windows.h>
+#include <fstream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+int main() {
+
+}
