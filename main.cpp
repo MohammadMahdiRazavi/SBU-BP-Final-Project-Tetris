8bfe756892f2dfbb5a613afb7581d94f0382434a
MohammadMahdiRazavi <mohammed.razavi1384@gmail.com>
2024-08-08 03:54:05 -0700

    colors is done!!!

--- main.cpp
+++ main.cpp
@@ -28,6 +28,16 @@ struct gameHistory{
     int time;
 };
 
+#define RESET "\033[0m"
+#define BLACK "\033[30m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define PURPLE "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+
 int count_of_game = 0;
 
 void menu(int** game, int row, int column, gameHistory leaderboard[100]){
