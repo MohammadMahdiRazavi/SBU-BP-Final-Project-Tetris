e4270c2f454a88a8276b9c65e237014b773ddb0b
MohammadMahdiRazavi <mohammed.razavi1384@gmail.com>
2024-08-08 03:24:24 -0700

    body of menu function is done

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <chrono>
 #include <thread>
+#include <unistd.h>
 
 using namespace std;
 
@@ -15,6 +16,35 @@ struct gameHistory{
     int score;
     int time;
 };
+
+void menu(int** game, int row, int column, gameHistory leaderboard[100]){
+    cout << "Hi ! Welcome to Tetris";
+    sleep(1000);
+    system("cls");
+    while (true){
+        cout << "1 - New Game";
+        cout << "2 - Leaderboard";
+        cout << "3 - How to playing Tetris";
+        cout << "4 - Exit";
+        int section;
+        cin >> section;
+        if(section == 1){
+
+        }
+        else if(section == 2){
+
+        }
+        else if(section == 3){
+
+        }
+        else if(section == 4){
+
+        }
+        else{
+
+        }
+    }
+}
 
 int main() {
 
